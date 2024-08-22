f7641ffc21bb806c3f3847228d0be9ffce6dee6e
Баглан Сариев <baglan@MacBook-Pro-Baglan.local>
2024-08-22 18:43:21 +0500

    Add 1C order creation method

--- src/app/Services/OrderService.php
+++ src/app/Services/OrderService.php
@@ -4,11 +4,21 @@ namespace App\Services;
 
 use App\Services\Service;
 use App\Repositories\OrderRepository;
+use App\Models\Catalog\Order;
+use App\Services\Integration\OneCApiService;
+use App\Helpers\Adapters\OrderAdapter;
 
 class OrderService extends Service
 {
     public function __construct(
-        protected OrderRepository $repository
+        protected OrderRepository $repository,
+        protected OneCApiService $oneCApiService
     ) {}
 
+    public function sendOrderToOneC(Order $order): array
+    {
+        return $this->oneCApiService->createOrder(
+            OrderAdapter::adaptOneCOrder($order)
+        );
+    }
 }
