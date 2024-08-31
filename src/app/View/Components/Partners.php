46eddd73185bd5b7c6d4f71dcd8f407d6c15336e
Баглан Сариев <baglan@MacBook-Pro-Baglan.local>
2024-08-31 23:56:03 +0500

    feature/main_page: Add partners block to main page

--- src/app/View/Components/Partners.php
+++ src/app/View/Components/Partners.php
@@ -5,22 +5,24 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use App\Services\GalleryService;
 
 class Partners extends Component
 {
     /**
      * Create a new component instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        private GalleryService $galleryService,
+    ) {}
 
     /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
     {
-        return view('components.partners');
+        return view('components.partners', [
+            'gallery' => $this->galleryService->getPartners()
+        ]);
     }
 }
