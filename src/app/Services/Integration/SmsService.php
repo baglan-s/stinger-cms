64453d0d45c1654438cc939009e4f9d77087438c
Arman <advanced315@gmail.com>
2024-09-02 21:00:02 +0500

    feature/authorization: #added SmsService class

---
+++ src/app/Services/Integration/SmsService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Integration;
+
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+class SmsService extends Service
+{
+    /**
+     * 
+     * @return string|int 
+     * @throws BindingResolutionException 
+     * @throws NotFoundExceptionInterface 
+     * @throws ContainerExceptionInterface 
+     */
+    public function generateRandomCode()
+    {
+        return rand(1000, 9999);
+    }
+}
