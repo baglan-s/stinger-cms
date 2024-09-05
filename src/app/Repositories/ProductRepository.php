cdab69a99941eae42a5ec637ccd1a8e0e34ec29d
Баглан Сариев <baglan@MacBook-Pro-Baglan.local>
2024-09-05 10:56:24 +0500

    feature/favourite_products: Add favourite products feature

--- src/app/Repositories/ProductRepository.php
+++ src/app/Repositories/ProductRepository.php
@@ -4,10 +4,21 @@ namespace App\Repositories;
 
 use App\Models\Catalog\Product;
 use App\Repositories\Repository;
+use Illuminate\Support\Facades\Cookie;
 
 class ProductRepository extends Repository
 {
     protected $model = Product::class;
+
+    private array $favouriteProductIds = [];
+
+    public function __construct()
+    {
+        $this->favouriteProductIds = json_decode(
+            Cookie::get('favourite_products', json_encode([])),
+            true
+        );
+    }
 
     public function latest(int $limit = 10)
     {
@@ -33,5 +44,52 @@ class ProductRepository extends Repository
             ->whereNull('parent_id')
             ->limit($limit)
             ->get();
+    }
+
+    public function getFavouriteProductIds()
+    {
+        return $this->favouriteProductIds;
+    }
+
+    public function addFavouriteProductId(int $productId)
+    {
+        $this->favouriteProductIds[] = $productId;
+
+        Cookie::queue('favourite_products', json_encode($this->favouriteProductIds), 43200);
+    }
+
+    public function removeFavouriteProductId(int $productId)
+    {
+        $key = array_search($productId, $this->favouriteProductIds);
+
+        if ($key !== false) {
+            unset($this->favouriteProductIds[$key]);
+        }
+
+        Cookie::queue('favourite_products', json_encode($this->favouriteProductIds), 43200);
+    }
+
+    public function clearFavouriteProductIds()
+    {
+        $this->favouriteProductIds = [];
+        Cookie::queue('favourite_products', json_encode([]), -1);
+    }
+
+    public function getFavouriteProducts($limit = null)
+    {
+        $favourites = $this->model()
+            ->whereIn('id', $this->favouriteProductIds);
+
+        return $limit ? $favourites->limit($limit)->get() : $favourites->get();
+    }
+
+    public function hasFavouriteProducts(): bool
+    {
+        return $this->favouriteProductsCount() > 0;
+    }
+
+    public function favouriteProductsCount()
+    {
+        return count($this->favouriteProductIds);
     }
 }
