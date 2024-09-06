69c834d45295a34d085f025e3daa11ba434df157
Баглан Сариев <baglan@MacBook-Pro-Baglan.local>
2024-09-06 12:55:51 +0500

    fixed lang error

--- src/app/Filament/Resources/UserResource.php
+++ src/app/Filament/Resources/UserResource.php
@@ -93,7 +93,7 @@ class UserResource extends Resource
 
                 Section::make('Roles')
                     ->description(__('admin.crud.create.choose_roles'))
-                    ->label(__('admin.crud.create.roles'))
+                    ->label(__('admin.crud.create.roles.title'))
                     ->schema([
                         CheckboxList::make('roles')
                             ->label(__('admin.crud.create.roles'))
