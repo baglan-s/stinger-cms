2031420b299921ef057c9937ec45c39aaa0a5791
Баглан Сариев <baglan@MacBook-Pro-Baglan.local>
2024-09-24 12:58:01 +0500

    cart in process

---
+++ src/app/Livewire/Catalog/Cart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Catalog;
+
+use Livewire\Component;
+
+class Cart extends Component
+{
+    public function render()
+    {
+        return view('livewire.catalog.cart');
+    }
+}
---
+++ src/app/Livewire/Catalog/CartModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Catalog;
+
+use Livewire\Component;
+
+class CartModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.catalog.cart-modal');
+    }
+}
