d64114ad5dbcc7caf9a826becd1253d7cbccd89c
Баглан Сариев <baglan@MacBook-Pro-Baglan.local>
2024-09-25 19:24:43 +0500

    cart modal in process 2

--- src/app/Livewire/Catalog/CartModal.php
+++ src/app/Livewire/Catalog/CartModal.php
@@ -3,9 +3,28 @@
 namespace App\Livewire\Catalog;
 
 use Livewire\Component;
+use App\Services\ProductService;
 
 class CartModal extends Component
 {
+    private ProductService $productService;
+
+    public bool $hasProduct = false;
+
+    public int $cartProductsCount = 0;
+
+    public $products;
+
+    public function mount()
+    {
+        
+    }
+
+    public function __construct()
+    {
+        $this->productService = app(ProductService::class);
+    }
+
     public function render()
     {
         return view('livewire.catalog.cart-modal');
